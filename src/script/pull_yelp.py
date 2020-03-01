252960ec5dde22fb7703a82a1a267de0ab244cc3
KShih <kontonsine@gmail.com>
2020-03-01 02:25:29 -0500

    import data to dynamo

--- src/script/pull_yelp.py
+++ src/script/pull_yelp.py
@@ -10,21 +10,23 @@ HEADERS = {'Authorization': 'bearer %s' % API_KEY}
 
 data = {}
 ids = []
-for i in range(0, 1000, 50):
-    PARAMETERS = {'term': 'indian restaurant',
-                  'limit': 50,
-                  'location': 'New York',
-                  'offset': i}
-    response = requests.get(url=ENDPOINT, params=PARAMETERS, headers=HEADERS)
-    temp_data = response.json()
-    print("Retrieved data from offset {}".format(i))
-    for item in temp_data['businesses']:
-        ids.append(item)
+types = ['french', 'indian', 'japanese', 'chinese', 'korean']
+for type in types:
+    for i in range(0, 1000, 50):
+        PARAMETERS = {'term': type + ' restaurant',
+                      'limit': 50,
+                      'location': 'New York',
+                      'offset': i}
+        response = requests.get(url=ENDPOINT, params=PARAMETERS, headers=HEADERS)
+        temp_data = response.json()
+        print("Retrieved data from offset {}".format(i))
+        for item in temp_data['businesses']:
+            ids.append(item)
 
-data['businesses'] = ids
+    data['businesses'] = ids
 
-# data = data['businesses'][0]
+    # data = data['businesses'][0]
 
-f = open("european_data.json", mode='w')
-json.dump(data, f)
-f.close()
+    f = open(type + "_data.json", mode='w')
+    json.dump(data, f)
+    f.close()
