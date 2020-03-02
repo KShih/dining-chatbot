c72d4cc991f812002207efddc8a5e55025f665b0
KShih <kontonsine@gmail.com>
2020-03-02 01:38:04 -0500

    get data from dynamodb using id in es

---
+++ src/lambda/get_suggestion.py
@@ -0,0 +1,87 @@
+"""
+Read the cuisine from SQS that the user spicify in Lex,
+Prepushing all the data into dynamo, creating mapping index on ES
+Use as lambda to search id in elastic search, and use it to get data from dynamo
+"""
+from __future__ import print_function # Python 2/3 compatibility
+import json
+import random
+import urllib3
+import boto3
+
+""" ---  Get the id from es --- """
+
+ENDPOINT = "https://search-restaurants-kcmb5vcpfi2wp6m25xvuy2ht5m.us-east-1.es.amazonaws.com/"
+INDEX = "restaurants/"
+ES_SEARCH_RESULT_SIZE = 10  # the size of the data we expect to be returned from es, set it little to ease the server loading
+
+
+def get_esquery_count(cuisine):
+    base_url = ENDPOINT+INDEX
+    count_url = base_url + '_count?q=' + cuisine
+    try:
+        http = urllib3.PoolManager()
+        result = http.request('GET', count_url).data  # result is binary data
+        data = json.loads(result.decode('utf-8'))
+        return data['count']
+    except:
+        None
+
+
+def get_esquery_data(cuisine, start):
+    base_url = ENDPOINT + INDEX
+    search_url = base_url + '_search?q=' + cuisine + "&from=" + str(start) + "&size=" + str(ES_SEARCH_RESULT_SIZE)
+    try:
+        http = urllib3.PoolManager()
+        result = http.request('GET', search_url).data  # result is binary data
+        data = json.loads(result.decode('utf-8'))
+        return data
+    except:
+        return None
+
+
+def get_id_from_hits(hits):
+    return [hit['_source']['id'] for hit in hits]
+
+
+""" ---  Get the data from dynamo --- """
+
+
+def get_dbdetail_by_id(ids):
+    client = boto3.client('dynamodb')
+    restaurants = []
+
+    for id in ids:
+        data = client.get_item(TableName='yelp-restaurants',
+                               Key={
+                                   'id': {'S': id}
+                               })
+        data = data['Item']
+        name, address, rating, phone = "", "", "", ""
+        if 'name' in data:
+            name = data['name']['S']
+        if 'address' in data:
+            address = data['address']['S']
+        if 'rating' in data:
+            rating = data['rating']['S']
+        if 'phone' in data:
+            phone = data['phone']['S']
+        restaurants.append([name, address, rating, phone])
+
+    restaurants.sort(key=lambda x:x[2], reverse=True)  # sort by rating
+    return restaurants
+
+
+if __name__ == '__main__':
+
+    """ 1. get es data """
+    cuisine = "chinese"
+    count = get_esquery_count(cuisine)
+    start = random.randrange(count - ES_SEARCH_RESULT_SIZE - 1)
+    data = get_esquery_data(cuisine, start)
+    hits = data['hits']['hits']
+    ids = get_id_from_hits(hits)
+
+    """ 2. get dynamo data """
+    restaurants = get_dbdetail_by_id(ids)
+    print(restaurants)
