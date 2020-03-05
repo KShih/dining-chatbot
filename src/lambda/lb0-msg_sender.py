60c4bdac5b4d21462bd7290dece1c94761a9e035
KShih <kontonsine@gmail.com>
2020-03-05 15:30:25 -0500

    LB0-msgSender to communicate to lex

--- src/lambda/lb0-msg_sender.py
+++ src/lambda/lb0-msg_sender.py
@@ -1,21 +1,14 @@
-import json
+import boto3
+
 
 def lambda_handler(event, context):
-    # TODO implement
-    return {
-        
-        'statusCode': 200,
-        'body': {
-                  "messages": [
-                    {
-                      "type": "string",
-                      "unstructured": {
-                        "id": 0,
-                        "text": "Hi, this is testing message",
-                        "timestamp": "string"
-                      }
-                    }
-                  ]
-                }
-    }
+    client = boto3.client('lex-runtime')
+    text = event['messages'][0]['unstructured']['text']
+    response = client.post_text(
+        botName='BookResturant',
+        botAlias='$LATEST',
+        userId='string',
+        inputText=text
+    )
 
+    return response
